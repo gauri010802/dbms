22dc5ee1697245bf54e1f31e2fea24364f134ece
gauri010802 <72457426+gauri010802@users.noreply.github.com>
2021-05-12 18:48:30 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet"https="//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+    
+    <form method="post" action="index.php">
+    <div class="log-box">
+        <h1>LOGIN</h1>
+        <div class="textbox">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <input type="text" STYLE="color: wheat" placeholder="USERNAME" name="username" >
+        </div>
+        <div class="textbox">
+            <i class="fa fa-key" aria-hidden="true"></i>
+            <input type="password" placeholder="PASSWORD" name="password" >
+        </div>
+        <button type="submit"class="btn" value="go" >
+          <nav><a href="index.php">go</a></nav>
+        </button>
+</div>
+   </form>
+</body>  
+</html>
